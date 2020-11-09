539c7eaeb4992cb59047959331d5f7b9bd6cd927
Julian Paolo Thiry <julianpaolothiry@gmail.com>
2020-11-08 18:42:07 -0600

    Fix wayward tabs

--- rtl/keyboard.sv
+++ rtl/keyboard.sv
@@ -50,69 +50,69 @@ behave in the most intuitive way possible. This can be boiled down to five
 overall goals:
 
 1. The A-Z, 0-9, and SPACE keys must behave exactly as they do on a real
-	Spectrum. This is the easy one.
+   Spectrum. This is the easy one.
 
 2. Both Shift keys must work as CAPS SHIFT, both Ctrl keys as SYMBOL SHIFT, and
    both Enter keys as ENTER. These keys must behave as if physically wired
-	together. I.E., the following sequence must produce a SHIFT Q:
-		Right Shift down, Left Shift down, Right Shift up, Q
+   together. I.E., the following sequence must produce a SHIFT Q:
+      Right Shift down, Left Shift down, Right Shift up, Q
 
 Note that if 1 and 2 are met, this would be a fully functional driver, as every
 Spectrum input can be accomplished. But we can do better!
-		
+      
 3. Each additional key on the Spectrum+ keyboard that is wired to CAPS SHIFT
-	must have an analogous key. These keys must behave as if they are physically
-	wired together in	the same way. In particular, pressing and releasing
-	different combinations of these keys with the keys they are wired to	must
-	not cause any unexpected signals.
-	These keys have been mapped as follows:
-		BREAK        (CS+Space)  Escape
-		EXTEND MODE  (CS+SS)     Tab
-		EDIT         (CS+1)      Home
-		CAPS LOCK    (CS+2)      Caps Lock
-		TRUE VIDEO   (CS+3)      Page Up
-		INV VIDEO    (CS+4)      Page Down
-		Left Arrow   (CS+5)      Left Arrow or Numpad 4
-		Down Arrow   (CS+6)      Down Arrow or Numpad 2
-		Up Arrow     (CS+7)      Up Arrow or Numpad 8
-		Right Arrow  (CS+8)      Right Arrow or Numpad 6
-		GRAPH        (CS+9)      End
-		DELETE       (CS+0)      BACKSPACE
+   must have an analogous key. These keys must behave as if they are physically
+   wired together in the same way. In particular, pressing and releasing
+   different combinations of these keys with the keys they are wired to must
+   not cause any unexpected signals.
+   These keys have been mapped as follows:
+      BREAK        (CS+Space)  Escape
+      EXTEND MODE  (CS+SS)     Tab
+      EDIT         (CS+1)      Home
+      CAPS LOCK    (CS+2)      Caps Lock
+      TRUE VIDEO   (CS+3)      Page Up
+      INV VIDEO    (CS+4)      Page Down
+      Left Arrow   (CS+5)      Left Arrow or Numpad 4
+      Down Arrow   (CS+6)      Down Arrow or Numpad 2
+      Up Arrow     (CS+7)      Up Arrow or Numpad 8
+      Right Arrow  (CS+8)      Right Arrow or Numpad 6
+      GRAPH        (CS+9)      End
+      DELETE       (CS+0)      BACKSPACE
 
 4. Keys bearing symbols that can be produced on a Spectrum using the SYMBOL
-	SHIFT key must produce those very symbols. This means that all of the
-	following keys	must act as if they are wired directly to SYMBOL SHIFT and
-	the appropriate second key:
-		-_  =+  ;:  '"(US) '@(UK)  ,<  .>  /?  #~(UK) Num/  Num*  Num-  Num+
-	For the first eight, a Shifted keypress must also send the appropriate keys.
-	Additionally, toggling Shift after pressing any of these keys must not cause
-	the Spectrum to see a different key being	pressed. For example, pressing
-	Shift and ,< will send SS+R for the "<" symbol. Releasing Shift must not
-	then cause SS+N (the "," symbol) to be sent, as this is clearly not the
-	intent of the user.
+   SHIFT key must produce those very symbols. This means that all of the
+   following keys must act as if they are wired directly to SYMBOL SHIFT and
+   the appropriate second key:
+      -_  =+  ;:  '"(US) '@(UK)  ,<  .>  /?  #~(UK) Num/  Num*  Num-  Num+
+   For the first eight, a Shifted keypress must also send the appropriate keys.
+   Additionally, toggling Shift after pressing any of these keys must not cause
+   the Spectrum to see a different key being pressed. For example, pressing
+   Shift and ,< will send SS+R for the "<" symbol. Releasing Shift must not
+   then cause SS+N (the "," symbol) to be sent, as this is clearly not the
+   intent of the user.
 
 Many might be tempted to stop there, but this driver goes beyond!
 
 5. Keys bearing symbols that can be produced on a Spectrum in EXTEND MODE must
-	produce those very symbols. This includes the following keys:
-		[{  ]}  \|  `~(US) #~(UK)
-	The Shifted versions must work appropriately as well. Since there is no `
-	symbol on the Spectrum, this key will produce the only other extended mode
-	symbol, ©.
+   produce those very symbols. This includes the following keys:
+      [{  ]}  \|  `~(US)  #~(UK)
+   The Shifted versions must work appropriately as well. Since there is no `
+   symbol on the Spectrum, this key will produce the only other extended mode
+   symbol, ©.
 
-	Note that this feature is meant as a convenience only. These symbols require
-	three distinct events in order to be processed correctly:
-		SYMBOL SHIFT + CAPS SHIFT
-		Release of CAPS SHIFT
-		SYMBOL SHIFT + Key
-	Because of this, it is necessary to hold the key down long enough for all
-	three events to occur. This is done quite quickly, but will definitely be
-	noticed by fast typists. If the keypress is too short, the Spectrum will go
-	into EXTEND MODE, but will not press the appropriate symbol key afterwards.
-	Note also that these keys will result in the wrong things appearing on the
-	screen if pressed when EXTEND MODE is already active. This means repeatedly
-	pressing these keys quickly will likely produce unexpected keywords. This is
-	NOT a bug, but merely the result of the combination of the prior two points.
+   Note that this feature is meant as a convenience only. These symbols require
+   three distinct events in order to be processed correctly:
+      SYMBOL SHIFT + CAPS SHIFT
+      Release of CAPS SHIFT
+      SYMBOL SHIFT + Key
+   Because of this, it is necessary to hold the key down long enough for all
+   three events to occur. This is done quite quickly, but will definitely be
+   noticed by fast typists. If the keypress is too short, the Spectrum will go
+   into EXTEND MODE, but will not press the appropriate symbol key afterwards.
+   Note also that these keys will result in the wrong things appearing on the
+   screen if pressed when EXTEND MODE is already active. This means repeatedly
+   pressing these keys quickly will likely produce unexpected keywords. This is
+   NOT a bug, but merely the result of the combination of the prior two points.
 
 Having been met, the above five goals should provide the ultimate combination
 of Spectrum-like behavior with additional functionality provided by all those
@@ -121,11 +121,11 @@ extra keys.
 In the above descriptions, there are a few references to (US) and (UK). This is
 because US and UK keyboard layouts differ as follows:
     Layout         OSD Mode
-	US    UK        US    UK
-	`~    `¬        ©~    
-	'"    '@        '"    '@
-	\|    #~        \|    #~
-	      \|              \|
+   US    UK        US    UK
+   `~    `¬        ©~    
+   '"    '@        '"    '@
+   \|    #~        \|    #~
+         \|              \|
 You should change the OSD settings to match your keyboard's layout if you want
 these keys to print what they display.
 
@@ -205,7 +205,7 @@ wire [25:0] sSymb = { s['h4A],s['h49],s['h41],s['h52],s['h4C],s['h55],s['h4E],s[
 
 wire  [4:0] keys[7:0];
 assign keys[0][0] = (rs & ~(s['h0E]))                            // In Recreated Spectrum mode, CAPS SHIFT is 0E
-                 | (~rs & ~((shift | |sCaps[15:0]) & ~(|sSymb[25:0]))); // Shift or any CAPS key, except if any SYMBOL key
+          | (~rs & ~((shift | |sCaps[15:0]) & ~(|sSymb[25:0]))); // Shift or any CAPS key, except if any SYMBOL key
 assign keys[0][1] = ~(s['h1A] | s['h1F]);                        // Z or Vrt:
 assign keys[0][2] = ~(s['h22]);                                  // X
 assign keys[0][3] = ~(s['h21] | s['h3F]);                        // C or Vrt?
@@ -235,7 +235,8 @@ assign keys[4][2] = ~(s['h3E] | s['h64] | s['h13]);              // 8 or Rght or
 assign keys[4][3] = ~(s['h3D] | s['h63] | s['h39] | s['h52]);    // 7 or Up or Num8 or '
 assign keys[4][4] = ~(s['h36] | s['h62] | s['h28]);              // 6 or Down or Num2
 
-assign keys[5][0] = ~(s['h4d] | (us & (s['h27] | (s['h08] & s['h0E])))); // P or US Vt"@ or US Vt`# plus `~
+assign keys[5][0] = ~(s['h4d]                                    // P or
+                      | (us & (s['h27] | (s['h08] & s['h0E])))); // US Vt"@ or US Vt`# plus `~
 assign keys[5][1] = ~(s['h44] | s['h4C]);                        // O or ;
 assign keys[5][2] = ~(s['h43]);                                  // I
 assign keys[5][3] = ~(s['h3C] | (s['h47] & s['h5B]));            // U or Vrt] plus ]}
